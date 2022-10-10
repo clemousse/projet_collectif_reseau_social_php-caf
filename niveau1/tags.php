9b57d932985ee4edce71d7c709b2271cf142f35f
clemousse <clemchasles0gmail.com>
2022-10-10 11:39:41 +0200

    fixed : tags appeared in the Mots-clés page when the user choose one of them

--- niveau1/tags.php
+++ niveau1/tags.php
@@ -17,15 +17,24 @@ INSERT INTO current_tag (id, name) VALUES (1,'default');-->
                 /**
                  * Etape 3: récupérer le nom du mot-clé
                  */
-                $choosenTag = $_POST['tag'];
-                // $updateSQL = "UPDATE current_tag SET name='$choosenTag'";
-                $laQuestionEnSql1 = "SELECT id FROM tags WHERE label='$choosenTag'";
-                $lesInformations = $mysqli->query($laQuestionEnSql1);
-                // $mysqli->query($updateSQL);
-                $tag = $lesInformations->fetch_assoc();
-                $tagId=$tag['id'];
-
-                
+                if(isset($_POST['tag'])) {
+                    $choosenTag = $_POST['tag'];
+                    //On update la valeur du tag courant par celui selectionné par l'utilisatrice => dans la table current_tag.
+                    $updateCurrentTag = "UPDATE current_tag SET label = '$choosenTag' WHERE id=1";
+                    //On sélectionne tous les tags dont l'id correspond au tag courant => dans la table tags
+                    $mysqli->query($updateCurrentTag);
+                    $laQuestionEnSql1 = "SELECT id FROM tags WHERE label='$choosenTag'";
+                    $lesInformations = $mysqli->query($laQuestionEnSql1);
+                    $tag = $lesInformations->fetch_assoc();
+                    $tagId=$tag['id'];
+                } else if (!isset($_POST['tag'])) {
+                    $laQuestionEnSql1 = "
+                    SELECT tags.id FROM tags INNER JOIN current_tag ON tags.label = current_tag.label;
+                    ";
+                    $lesInformations = $mysqli->query($laQuestionEnSql1);
+                    $tag = $lesInformations->fetch_assoc();
+                    $tagId=$tag['id'];
+                }
                 include('photo.php');
                 ?>
             </aside>
