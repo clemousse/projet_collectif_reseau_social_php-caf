aa26fa0ddc78484dc2fa0f5885c2336b0192a091
clemousse <clemchasles0gmail.com>
2022-10-17 10:21:18 +0200

    fix : change data for connection

--- niveau1/connection.php
+++ niveau1/connection.php
@@ -1,7 +1,6 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "MiniChou1701#", "socialnetwork");
-if ($mysqli->connect_errno)
-    {
-        echo("Échec de la connexion : " . $mysqli->connect_error);
-        exit();
-    }
+$mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
+if ($mysqli->connect_errno) {
+    echo ("Échec de la connexion : " . $mysqli->connect_error);
+    exit();
+}
